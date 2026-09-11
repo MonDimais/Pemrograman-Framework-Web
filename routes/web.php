6b20c7733db6b1327c8642f023378ae1b5377929
Khanzaetha <khanzaetha@mondimaiss.didimdamdar.com>
2026-09-11 19:26:15 +0700

    Add Closure Function

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'Toko Koperasi KOPDES Desa adalah toko yang menyediakan pengalaman belanja seperti dalam video game receh.';
+});
